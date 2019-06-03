1ab18d8fea8367ef200cc1bb4f6f3b30eee7601d
saulo <saulo@saulo-ultra>
2019-06-02 23:33:15 -0300

    - Início do Projeto com Spring Boot;

---
+++ src/main/java/br/com/alura/listavip/ConvidadoController.java
@@ -0,0 +1,18 @@
+package br.com.alura.listavip;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ConvidadoController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@RequestMapping("/lista")
+	public String listaConvidados() {
+		return "listaconvidados";
+	}
+}
