b853ce161b637a4cb835c65b84cac41dd719aa78
saulo <saulo@saulo-ultra>
2019-06-03 21:35:19 -0300

    - CRUD genérico e spring devtools;

--- src/main/java/br/com/alura/listavip/ConvidadoController.java
+++ src/main/java/br/com/alura/listavip/ConvidadoController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.com.alura.listavip.model.Convidado;
 import br.com.alura.listavip.repository.ConvidadoRepository;
@@ -23,5 +25,19 @@ public class ConvidadoController {
 		Iterable<Convidado> convidados = repository.findAll();
 		model.addAttribute("convidados", convidados);
 		return "listaconvidados";
+	}
+
+	@RequestMapping(value = "salvar", method = RequestMethod.POST)
+	public String salvar(Model model, @RequestParam("nome") String nome, @RequestParam("email") String email,
+			@RequestParam("telefone") String telefone) {
+		Convidado novoConvidado = new Convidado(nome, email, telefone);
+		repository.save(novoConvidado);
+		Iterable<Convidado> convidados = repository.findAll();
+		model.addAttribute("convidados", convidados);
+		return "listaconvidados";
+	}
+
+	public void obterConvidadoPor(String nome) {
+		repository.findByNome(nome);
 	}
 }
--- src/main/java/br/com/alura/listavip/model/Convidado.java
+++ src/main/java/br/com/alura/listavip/model/Convidado.java
@@ -2,13 +2,14 @@ package br.com.alura.listavip.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity(name = "convidado")
 public class Convidado {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
@@ -24,7 +25,13 @@ public class Convidado {
 
 	public Convidado() {
 		super();
-		// TODO Auto-generated constructor stub
+	}
+
+	public Convidado(String nome, String email, String telefone) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
 	}
 
 	public Long getId() {
