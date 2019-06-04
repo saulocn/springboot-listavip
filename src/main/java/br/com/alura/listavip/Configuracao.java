f0f80f6a5a7eb419e34200d7879428796708e319
saulo <saulo@saulo-ultra>
2019-06-03 23:04:19 -0300

    - Utilizando o container undertow; - Adicionando as configurações no arquivo application.properties;

--- src/main/java/br/com/alura/listavip/Configuracao.java
+++ src/main/java/br/com/alura/listavip/Configuracao.java
@@ -1,11 +1,7 @@
 package br.com.alura.listavip;
 
-import javax.sql.DataSource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @SpringBootApplication
 public class Configuracao {
@@ -14,13 +10,4 @@ public class Configuracao {
 		SpringApplication.run(Configuracao.class, args);
 	}
 
-	@Bean
-	public DataSource dataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/aluralistavip");
-		dataSource.setUsername("saulo");
-		dataSource.setPassword("123");
-		return dataSource;
-	}
 }
